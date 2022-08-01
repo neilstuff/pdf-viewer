b08a4d6d719a6d00a08058a5d33587ec467a5743
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2022-08-02 08:53:47 +1000

    First Commit

---
+++ main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const config = require('./config.json');
+
+const electron = require('electron');
+const { app } = electron;
+const { protocol } = electron;
+const { ipcMain } = electron;
+
+const BrowserWindow = electron.BrowserWindow;
+
+const mime = require('mime');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+const os = require('os');
+
+var mainWindow = null;
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({
+        width: (config.mode == "debug") ? 900 : 1000,
+        height: 600,
+        resizable: true,
+        frame: true,
+        maximizable: true,
+        minHeight: 600,
+        minWidth: (config.mode == "debug") ? 900 : 1000,
+        fullscreenable: true,
+        autoHideMenuBar: true,
+
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            webSecurity: false
+        }
+
+    });
+
+    if (config.mode == "debug") {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.setMenu(null);
+    mainWindow.setTitle('PDF Viewer');
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+
+}
+
+app.on('ready', () => {
+
+    createWindow();
+
+});
